4b1db14b39bb7de27cda69a481b5a65fe0d180fc
Simon Kärrman <simonkarrman@icloud.com>
2021-04-13 10:46:48 +0200

    Fix colors

--- visualizer/constants.py
+++ visualizer/constants.py
@@ -4,8 +4,8 @@ CLASS_FILE_PLACEHOLDER = "##PLACEHOLDER_CLASS_FILE##"
 
 
 class Color:
-    RED = '\033[91m'
-    BLUE = '\033[94m'
+    RED = '\033[31m'
+    BLUE = '\033[34m'
     BOLD = '\033[1m'
     MAGENTA = '\033[1;35m'
     UNDERLINE = '\033[4m'
